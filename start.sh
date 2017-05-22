7b535e537403abff5553cea111c8869edb464e00
Weslley Camilo <weslley.azevedo@ahgora.com.br>
2017-05-22 16:44:11 -0300

    adicionado confd

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+/usr/local/bin/confd -node=[etcdAddr] -watch -node http://${etcdAddr-etcd}:${etcdPort} &
+
 # Starts FPM
 nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
 
